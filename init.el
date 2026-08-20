a256efbbdff22bfb49b6642bbfcbeb1a6b587d43
korochan7 <guiter9993@yahoo.co.jp>
2026-08-20 23:15:01 +0900

    書籍の練習でinit.elにmy-modeを追加

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit ddskk)))
+ '(package-selected-packages '(csv-mode ddskk magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -38,4 +38,34 @@
 
 ;; 不正な入力があった場合ベルを鳴らさないようにする。
 (setq ring-bell-function 'ignore)
+
+(setq default-directory "C:/Users/guite/Documents/emacs/")
+
+;; 自分のメージャーモード
+(defun my-mode()
+  (interactive)
+  (setq major-mode 'my-mode
+	mode-name "私のモード")
+  (setq my-local-map (make-keymap))
+  (define-key my-local-map "h" 'backward-char)
+  (define-key my-local-map "j" 'next-line)
+  (define-key my-local-map "k" 'previous-line)
+  (define-key my-local-map "l" 'forward-char)
+  (define-key my-local-map "\C-ch" 'hello-world)
+  (use-local-map my-local-map))
+  
+(defun hello-world()
+  (interactive)
+  (insert "Hello, world!\n"))
+
+(defun my-mode2()
+  (interactive)
+  (setq major-mode 'my-mode2
+  	mode-name "私のモード２")
+  (setq my-local-map (make-keymap))
+  (define-key my-local-map "a" 'hello-world)
+  (use-local-map my-local-map)
+  )
+
+
 
